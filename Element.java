47e999ddc1edb5b60b30e83f8172fd1a638b662d
Gregor Robertson <gregor@btinternet.com>
2016-08-27 18:17:30 +0100

    now uses buttons and not command line for input

--- Element.java
+++ Element.java
@@ -1,32 +1,47 @@
+
+import javax.swing.JLabel;
+
 public class Element {
-  private int element;
 
-  public Element(int element){
-    this.element = element;
-  }
+    private int element;
+    private final JLabel label;
 
-  public void combine(Element e) {
-    if (this.canCombine(e)) {
-      element += e.getElement();
+    public Element(int element, JLabel label) {
+        this.label = label;
+        this.element = element;
     }
-  }
 
-  public boolean canCombine(Element e) {
-    return e.getElement() == element;
-  }
-
-  public int getElement(){ return element; }
-
-  @Override
-  public String toString(){
-    if(element < 10) {
-      return "000" + element;
-    } else if (element < 100) {
-      return "00" + element;
-    } else if (element < 1000) {
-      return "0" + element;
-    } else {
-      return "" + element;
+    public void combine(Element e) {
+        this.setElement(this.element + e.getElement());
+        e.setElement(0);
     }
-  }
+
+    public int getElement() {
+        return this.element;
+    }
+
+    public void setElement(int element) {
+        this.element = element;
+    }
+
+    public void updateLabel() {
+        this.label.setText(this.toString());
+    }
+
+    public JLabel getLabel() {
+        return this.label;
+    }
+
+    @Override
+    public String toString() {
+        if (this.element < 10) {
+            return "000" + this.element;
+        } else if (this.element < 100) {
+            return "00" + this.element;
+        } else if (this.element < 1000) {
+            return "0" + this.element;
+        } else {
+            return "" + this.element;
+        }
+    }
 }
--- Game.java
+++ Game.java
@@ -1,209 +1,246 @@
-import java.util.Scanner;
+
 import java.util.Random;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import java.awt.GridLayout;
-import javax.swing.JLabel;
+public class Game extends javax.swing.JFrame {
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+    /**
+     * Command buttons. Used to move pieces on the board.
+     */
+    private javax.swing.JButton upButton;
+    private javax.swing.JButton downButton;
+    private javax.swing.JButton leftButton;
+    private javax.swing.JButton rightButton;
 
-public class Game extends JFrame {
+    /**
+     * Board. boardDisplay is a 2D array of the labels used to display the
+     * elements on the board.
+     */
+    private Element[][] board;
 
-  Element[][] board = new Element[4][4];
-
-  JPanel panel = new JPanel();
-  JButton b = new JButton("Hello");
-  JButton test = new JButton("test");
-
-  public Game() {
-
-    super("2048");
-
-    setSize(600,600);
-    setResizable(false);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-  }
-
-  /*
-   * The functions related to the move up command.
-   * The canMoveUp checks that the element exists at the given location which can move up.
-   * moveUp will move up an element
-   * up is a loop which will check each column in turn to preform all valid moves
-  */
-  public boolean canMoveUp(int i, int j) {
-    return i > 0 && i < 4
-      && board[i][j] != null
-      && (board[i-1][j] == null || board[i][j].canCombine(board[i-1][j]));
-  }
-
-  public void moveUp(int i, int j) {
-    if (board[i-1][j] == null) {
-      board[i-1][j] = board[i][j];
-    } else {
-      board[i-1][j].combine(board[i][j]);
+    /**
+     * Creates new form Main
+     */
+    public Game() {
+        initComponents();
+        newTurn();
     }
-    board[i][j] = null;
-  }
 
-  public void up() {
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 4; j++) {
-        int tempI = i;
-        while (canMoveUp(tempI, j)) {
-          moveUp(tempI, j);
-          tempI--;
+    private void initComponents() {
+
+        upButton = new javax.swing.JButton();
+        rightButton = new javax.swing.JButton();
+        leftButton = new javax.swing.JButton();
+        downButton = new javax.swing.JButton();
+        board = new Element[4][4];
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        upButton.setText("Up");
+        upButton.addActionListener((java.awt.event.ActionEvent evt) -> {
+            upButtonActionPerformed(evt);
+        });
+
+        rightButton.setText("Right");
+        rightButton.addActionListener((java.awt.event.ActionEvent evt) -> {
+            rightButtonActionPerformed(evt);
+        });
+
+        leftButton.setText("Left");
+        leftButton.addActionListener((java.awt.event.ActionEvent evt) -> {
+            leftButtonActionPerformed(evt);
+        });
+
+        downButton.setText("Down");
+        downButton.setToolTipText("");
+        downButton.addActionListener((java.awt.event.ActionEvent evt) -> {
+            downButtonActionPerformed(evt);
+        });
+
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                board[x][y] = new Element(0, new javax.swing.JLabel());
+            }
         }
-      }
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createSequentialGroup()
+                                        .addComponent(board[0][0].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[1][0].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[2][0].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[3][0].getLabel()))
+                                .addGroup(layout.createSequentialGroup()
+                                        .addComponent(board[0][1].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[1][1].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[2][1].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[3][1].getLabel()))
+                                .addGroup(layout.createSequentialGroup()
+                                        .addComponent(board[0][2].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[1][2].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[2][2].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[3][2].getLabel()))
+                                .addGroup(layout.createSequentialGroup()
+                                        .addComponent(board[0][3].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[1][3].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[2][3].getLabel())
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(board[3][3].getLabel())))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(leftButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(downButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(upButton, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(rightButton)
+                        .addContainerGap(21, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createSequentialGroup()
+                                        .addComponent(upButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(rightButton)
+                                                .addComponent(leftButton))
+                                        .addGap(7, 7, 7)
+                                        .addComponent(downButton))
+                                .addGroup(layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(board[0][3].getLabel())
+                                                .addComponent(board[1][3].getLabel())
+                                                .addComponent(board[2][3].getLabel())
+                                                .addComponent(board[3][3].getLabel()))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(board[0][2].getLabel())
+                                                .addComponent(board[1][2].getLabel())
+                                                .addComponent(board[2][2].getLabel())
+                                                .addComponent(board[3][2].getLabel()))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(board[0][1].getLabel())
+                                                .addComponent(board[1][1].getLabel())
+                                                .addComponent(board[2][1].getLabel())
+                                                .addComponent(board[3][1].getLabel()))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(board[0][0].getLabel())
+                                                .addComponent(board[1][0].getLabel())
+                                                .addComponent(board[2][0].getLabel())
+                                                .addComponent(board[3][0].getLabel()))))
+                        .addContainerGap(22, Short.MAX_VALUE))
+        );
+
+        pack();
     }
-  }
 
-  /*
-   * The functions related to move down command
-  */
-  public boolean canMoveDown(int i, int j) {
-    return i < 3 && i > -1
-      && board[i][j] != null
-      && (board[i+1][j] == null || board[i][j].canCombine(board[i+1][j]));
-  }
-
-  public void moveDown(int i, int j) {
-    if (board[i+1][j] == null) {
-      board[i+1][j] = board[i][j];
-    } else {
-      board[i+1][j].combine(board[i][j]);
-    }
-    board[i][j] = null;
-  }
-
-  public void down() {
-    for (int i = 3; i >= 0; i--) {
-      for (int j = 3; j >= 0; j--) {
-        int tempI = i;
-        while (canMoveDown( tempI, j)) {
-          moveDown(tempI, j);
-          tempI++;
+    private void newTurn() {
+        Random random = new Random();
+        int x;
+        int y;
+        do {
+            x = random.nextInt(4);
+            y = random.nextInt(4);
+        } while (board[x][y].getElement() != 0);
+        board[x][y].setElement((1 + random.nextInt(2)) * 2);
+        for (x = 0; x < 4; x++) {
+            for (y = 0; y < 4; y++) {
+                board[x][y].updateLabel();
+            }
         }
-      }
     }
-  }
 
-
-  /*
-   * The functions related to move left command,
-  */
-  public boolean canMoveLeft(int i, int j) {
-    return j > 0 && j < 4
-    && board[i][j] != null
-    && (board[i][j-1] == null || board[i][j].canCombine(board[i][j-1]));
-  }
-
-  public void moveLeft(int i, int j) {
-    if (board[i][j-1] == null) {
-      board[i][j-1] = board[i][j];
-    } else {
-      board[i][j-1].combine(board[i][j]);
-    }
-    board[i][j] = null;
-  }
-
-  public void left() {
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 4; j++) {
-        int tempJ = j;
-        while (canMoveLeft(i, tempJ)) {
-          moveLeft(i, tempJ);
-          tempJ--;
+    private void upButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        for (int x = 0; x < 4; x++) {
+            for (int y = 3; y > -1; y--) {
+                int tempY = y;
+                while (tempY < 3 && (board[x][tempY + 1].getElement() == 0 || board[x][tempY].getElement() == board[x][tempY + 1].getElement())) {
+                    board[x][tempY + 1].combine(board[x][tempY]);
+                    tempY++;
+                }
+            }
         }
-      }
+        newTurn();
     }
-  }
 
-  /*
-   * The functions related to move right command
-  */
-  public boolean canMoveRight( int i, int j) {
-    return j < 3 && j > -1
-    && board[i][j] != null
-    && (board[i][j+1] == null || board[i][j].canCombine(board[i][j+1]));
-  }
-
-  public void moveRight( int i, int j) {
-    if (board[i][j+1] == null) {
-      board[i][j+1] = board[i][j];
-    } else {
-      board[i][j+1].combine(board[i][j]);
-    }
-    board[i][j] = null;
-
-  }
-
-  public void right() {
-    for (int i = 3; i >= 0; i--) {
-      for (int j = 3; j >= 0; j--) {
-        int tempJ = j;
-        while (canMoveRight( i, tempJ)) {
-          moveRight( i, tempJ);
-          tempJ++;
+    private void downButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                int tempY = y;
+                while (tempY > 0 && (board[x][tempY - 1].getElement() == 0 || board[x][tempY].getElement() == board[x][tempY - 1].getElement())) {
+                    board[x][tempY - 1].combine(board[x][tempY]);
+                    tempY--;
+                }
+            }
         }
-      }
+        newTurn();
     }
 
-  }
-
-
-  public static void main(String args[]) {
-    Game game = new Game();
-    Scanner user = new Scanner(System.in);
-    Random random = new Random();
-    int x;
-    int y;
-    String move;
-    System.out.println("Type up, down, left or right to move the elements in the respective"
-                          + "directions");
-    while(true){
-      do {
-        x = random.nextInt(4);
-        y = random.nextInt(4);
-      } while (game.board[x][y] != null);
-      game.board[x][y] = new Element((1 + random.nextInt(2)) * 2);
-      game.panel = new JPanel();
-      game.panel.setLayout(new GridLayout(4, 4));
-      for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-          if (game.board[i][j] != null) {
-            game.panel.add(new JLabel((game.board[i][j].toString() + " ")));
-          } else {
-            game.panel.add(new JLabel(("0000 ")));
-          }
+    private void leftButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                int tempX = x;
+                while (tempX > 0 && (board[tempX - 1][y].getElement() == 0 || board[tempX][y].getElement() == board[tempX - 1][y].getElement())) {
+                    board[tempX - 1][y].combine(board[tempX][y]);
+                    tempX--;
+                }
+            }
         }
-      }
-      game.setContentPane(game.panel);
-      game.setVisible(true);
-      System.out.println("\n Your next move? ");
-      move = user.next();
-      move = move.toLowerCase();
-      switch(move) {
-        case "up":
-          game.up();
-          break;
-        case "down":
-          game.down();
-          break;
-        case "left":
-          game.left();
-          break;
-        case "right":
-          game.right();
-          break;
-        default:
-          System.out.println("Invalid move");
-          System.exit(0);
-      }
+        newTurn();
     }
 
-  }
+    private void rightButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        for (int y = 0; y < 4; y++) {
+            for (int x = 3; x > -1; x--) {
+                int tempX = x;
+                while (tempX < 3 && (board[tempX + 1][y].getElement() == 0 || board[tempX][y].getElement() == board[tempX + 1][y].getElement())) {
+                    board[tempX + 1][y].combine(board[tempX][y]);
+                    tempX++;
+                }
+            }
+        }
+        newTurn();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Game.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new Game().setVisible(true);
+        });
+    }
+
 }
